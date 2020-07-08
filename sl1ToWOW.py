29f070c0b7b0e691e397cdc414f68b913ab16eb6
PolSerg <43808302+PolSerg@users.noreply.github.com>
2020-07-08 14:06:43 +0300

    Update sl1ToWOW.py

--- sl1ToWOW.py
+++ sl1ToWOW.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 #    Copyright © 2020 Polyakov Sergey (PolSerg). All rights reserved.
+#    e-mail: polserg12x@mail.ru
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
